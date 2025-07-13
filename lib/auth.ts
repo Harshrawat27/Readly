18241d181d1230b639b6a6039977669089ccba1a
Harsh Rawat <harshrawat.dev@gmail.com>
2025-07-13 13:34:07 +0530

    basic setup of authentication

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,22 @@
 import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+// If your Prisma file is located elsewhere, you can change the path
+import { PrismaClient } from '@/lib/generated/prisma';
 
+const prisma = new PrismaClient();
 export const auth = betterAuth({
-  //...
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql', // or "mysql", "postgresql", ...etc
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
 });
