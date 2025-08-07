822cd419be6843ee07d3dd69d7867edab2633adb
Harsh Rawat <harshrawat.dev@gmail.com>
2025-08-08 01:30:46 +0530

    fixed file size upload via vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  api: {
+    bodyParser: {
+      sizeLimit: '50mb', // or '20mb', adjust as needed
+    },
+  },
 };
 
 export default nextConfig;
